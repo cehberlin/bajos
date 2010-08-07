226921dd6d2a0c6df2fcbc3a28fb2b14e18ec6a6
c.hrabia <c.hrabia@c12d0c00-9e51-c958-af8d-857a4e95901e>
2010-08-07 09:41:32 +0000

    CH: Bugfix scanFill

--- BAJOSBOOT/sources/java/graphics/DisplayHSB.java
+++ BAJOSBOOT/sources/java/graphics/DisplayHSB.java
@@ -11,7 +11,6 @@ public class DisplayHSB extends Display
     public DisplayHSB()
     {    	
     	super();
-    	System.out.println ("DisplayHSB() ...");
     	//Standard Vordergrundfarbe: schwarz
     	DisplayHSB.hue=255;
     	DisplayHSB.saturation=0;
@@ -21,13 +20,11 @@ public class DisplayHSB extends Display
 	public void drawPoint(Point p)
     {
     	//mit HSB
-		System.out.println ("DisplayHSB.drawPoint()");
 		System.platform.drawPointHSB(p);
     }
 
 	public void clearDisplay()
     {
-		System.out.println ("DisplayHSB.clearDisplay ...");
 		System.platform.clearZBuffer();
     	super.clearDisplay();
     }
@@ -35,14 +32,12 @@ public class DisplayHSB extends Display
     public void drawLine(Point pa, Point pe)
     {
     	//nach dem DDA-Alg.
-    	System.out.println ("DisplayHSB.drawLine()");
     	System.platform.drawLineHSB(pa, pe);
     }
        
 	
 	public static void setHSBColor(float hue, float saturation, float brightness)	
 	{
-		System.out.println ("DisplayHSB.setHSBColor()");
 		DisplayHSB.hue=hue;
 		DisplayHSB.saturation=saturation;
 		DisplayHSB.brightness=brightness;						
@@ -53,17 +48,13 @@ public class DisplayHSB extends Display
 		// to do: sort points !!!
 		// assume: points are sorted by rising y
 
-		System.out.println ("DisplayHSB.triangleFill()");
 		System.platform.drawTriangleFill(p0, p1, p2);
 	}
 	
 	public void drawPolyline(Polyline pl)						
 	{
-		System.out.println (pl.size());
 		for (int i=0;i < pl.size(); i++) 
 		{
-			System.out.println (i);
-			System.out.println ((i+1)%pl.size());
 			drawLine(pl.pointAt(i),pl.pointAt((i+1)%pl.size()));
 		}
 	}
@@ -77,20 +68,31 @@ public class DisplayHSB extends Display
 		Point	b	=  new Point(); 
 		
 		for (int i=0; i < pl.size();i++)
+		{
 			if ((int)pl.pointAt(i).y != (int)pl.pointAt((i+1)%pl.size()).y)
 				if (pl.pointAt(i).y > pl.pointAt((i+1)%pl.size()).y)
 					al.addElement(new Line(pl.pointAt(i),pl.pointAt((i+1)%pl.size())));
 			else			// eintragen der lines in Vektor, richtig rum
 				al.addElement(new Line(pl.pointAt((i+1)%pl.size()),pl.pointAt(i)));
+		}
+		
 		// Parallelen zur y-Achse weglassen
 		for (int j=0; j < al.size();j++)
+		{
 			for (int i=1; i < al.size(); i++)	// sortieren der Lines nach y-Gr?e
+			{
 				if (((Line)al.elementAt(i-1)).anf.y < ((Line)al.elementAt(i)).anf.y) 
 				{
 					l=(Line)al.elementAt(i-1); 
-					al.remove(i-1);
-					al.set(i,l);
+					al.removeElementAt(i-1);
+					if (i==al.size())
+						al.add(l);
+					else
+						al.insertElementAt(l,i);
 				}
+			}
+		}
+		
 		int y=(int)((Line)al.elementAt(0)).anf.y;
 		for (int i=0;i< al.size(); i++)
 			if (y==(int)((Line)al.elementAt(i)).anf.y)
@@ -98,6 +100,7 @@ public class DisplayHSB extends Display
 			else break;  // da sie geordnet sind !!
 		if ((loi.size() %2)!=0)
 			 System.out.println("hier stimmt was nicht");
+
 		while (loi.size() !=0)
 		{
 			int	[] xs= new int[loi.size()];
@@ -151,17 +154,15 @@ public class DisplayHSB extends Display
 			}
 			y-=1;		// weiter hoch
 			for (int i=0; i < loi.size();i++)
-				if ((int)((Line)loi.elementAt(i)).end.y ==y) 
-				{
-					loi.remove(i);i--;if ( i < 0) i=-1;
-				}// entfernen uninteressanter lines
+				if ((int)((Line)loi.elementAt(i)).end.y ==y) {
+					loi.removeElementAt(i);i--;if ( i < 0) i=-1;}// entfernen uninteressanter lines
 				if (y >= yMax)
 					System.out.println("hier stimmt was nicht");
 				for (int i=0; i < al.size();i++)	// hinzufgen interessanter lines
 					if ((int)((Line)al.elementAt(i)).anf.y == y)
 					{
 						loi.addElement((Line)al.elementAt(i)/*!!*/);
-						al.remove(i);i--;if (i < 0) i=-1;
+						al.removeElementAt(i);i--;if (i < 0) i=-1;
 					} // aufessen
 			//else break;  // hier muss irgend was in der Reihenfolgen aufsteigend absteigend 
 			//in al nicht stimmen
