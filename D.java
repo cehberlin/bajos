d8e5462846ef5fcdb8d8efc5fb1755e12dbad580
c.hrabia <c.hrabia@c12d0c00-9e51-c958-af8d-857a4e95901e>
2010-08-07 08:53:17 +0000

    Added items remotely
    C:\Dokumente und Einstellungen\Stephan\Desktop\bajos\B.java
    C:\Dokumente und Einstellungen\Stephan\Desktop\bajos\bajosavr.c
    C:\Dokumente und Einstellungen\Stephan\Desktop\bajos\bajosavr.h
    C:\Dokumente und Einstellungen\Stephan\Desktop\bajos\C.java
    C:\Dokumente und Einstellungen\Stephan\Desktop\bajos\classfile.c
    C:\Dokumente und Einstellungen\Stephan\Desktop\bajos\classfile.h
    C:\Dokumente und Einstellungen\Stephan\Desktop\bajos\D.java
    C:\Dokumente und Einstellungen\Stephan\Desktop\bajos\definitions.h
    C:\Dokumente und Einstellungen\Stephan\Desktop\bajos\EEE.java
    C:\Dokumente und Einstellungen\Stephan\Desktop\bajos\FFF.java
    C:\Dokumente und Einstellungen\Stephan\Desktop\bajos\generateBootClassPackage.c
    C:\Dokumente und Einstellungen\Stephan\Desktop\bajos\GGG.java
    C:\Dokumente und Einstellungen\Stephan\Desktop\bajos\heap.c
    C:\Dokumente und Einstellungen\Stephan\Desktop\bajos\heap.h
    C:\Dokumente und Einstellungen\Stephan\Desktop\bajos\HHH.java
    C:\Dokumente und Einstellungen\Stephan\Desktop\bajos\interpreter.c
    C:\Dokumente und Einstellungen\Stephan\Desktop\bajos\interpreter.h
    C:\Dokumente und Einstellungen\Stephan\Desktop\bajos\license.terms
    C:\Dokumente und Einstellungen\Stephan\Desktop\bajos\limits.h
    C:\Dokumente und Einstellungen\Stephan\Desktop\bajos\makefile
    C:\Dokumente und Einstellungen\Stephan\Desktop\bajos\My.java
    C:\Dokumente und Einstellungen\Stephan\Desktop\bajos\native.c
    C:\Dokumente und Einstellungen\Stephan\Desktop\bajos\native.h
    C:\Dokumente und Einstellungen\Stephan\Desktop\bajos\ProducerConsumer.java
    C:\Dokumente und Einstellungen\Stephan\Desktop\bajos\scheduler.c
    C:\Dokumente und Einstellungen\Stephan\Desktop\bajos\scheduler.h
    C:\Dokumente und Einstellungen\Stephan\Desktop\bajos\stack.c
    C:\Dokumente und Einstellungen\Stephan\Desktop\bajos\stack.h
    C:\Dokumente und Einstellungen\Stephan\Desktop\bajos\T1.java
    C:\Dokumente und Einstellungen\Stephan\Desktop\bajos\T2.java
    C:\Dokumente und Einstellungen\Stephan\Desktop\bajos\typedefinitions.h
    C:\Dokumente und Einstellungen\Stephan\Desktop\bajos\A.java

---
+++ D.java
@@ -0,0 +1,12 @@
+/*
+* FHW-Berlin, Fachbereich Berufsakademie, Fachrichtung Informatik
+* See the file "license.terms" for information on usage and redistribution of this file.
+*/
+import java.lang.*;
+public class D {
+
+	public static void main(String [] args){
+	System.out.println("Hallo Welt");
+}
+
+}
