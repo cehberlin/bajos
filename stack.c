5c71bbe4ada5e34b3a2c46a5c3490d8474a1cecc
c.hrabia <c.hrabia@c12d0c00-9e51-c958-af8d-857a4e95901e>
2010-08-07 09:45:09 +0000

    nur comments

--- stack.c
+++ stack.c
@@ -12,53 +12,54 @@
 #include "definitions.h"
 #include "stack.h"
 
-// op stack hält  locals und operanden
-// method stack hält globale variable (cN, mN, local,..)
+// op stack holds  locals and operanden
+// method stack holds globale variable (cN, mN, local,..)
 static slot* 	opSp;
-static u2* 		methodSp;
+static u2* 	methodSp;
 
 void opStackInit(slot** m)	{		// per thread, fixed size 
 #if (LINUX||AVR8||AVR32LINUX)
 	if ((*m= (slot*) calloc((size_t)OPSTACKSIZE,sizeof(slot))) == NULL)
-			{printf("malloc error\n");exit(-1);}			
+			errorExit(-1,"op stack malloc error\n");			
 #else
 // classfiles - heap - (opstack methodstack)/ per thread
 //make it better
 *m=(slot*)((u4)apClassFileBase+MAXBYTECODE+4*MAXHEAP+numThreads*(4*OPSTACKSIZE+2*METHODSTACKSIZE));
 #endif
 }
-void opStackPush( slot val)				{	*(opSp++)=val;						}	
+void opStackPush( slot val)	{	*(opSp++)=val;			}	
 //  sp grothws with increasing addresses
-// and shows to TOS first free place
+// and shows to TOS -> first free place
 
-slot opStackPop()						{	return *(--opSp);					}	
+slot opStackPop()		{	return *(--opSp);		}	
 // operand stack stores 4 bytes
 
-slot  opStackPeek()						{	return *(opSp-1);					}
+slot  opStackPeek()		{	return *(opSp-1);		}
 
-void opStackPoke( slot val)				{	*(opSp-1)=val;						}
+void opStackPoke( slot val)	{	*(opSp-1)=val;			}
 
-void opStackSetValue(u2 pos, slot val)	{	*(opStackBase+pos)=val;				}
+void opStackSetValue(u2 pos, slot val)	{*(opStackBase+pos)=val;	}
  
-slot opStackGetValue(u2  pos)			{	return *(opStackBase+pos);			}
+slot opStackGetValue(u2  pos)	{	return *(opStackBase+pos);	}
 
-u2 opStackGetSpPos()					{  	return (opSp-opStackBase);		}	
+u2 opStackGetSpPos()		{  	return (opSp-opStackBase);	}	
 // relative to actual base
 
-void opStackSetSpPos(u2 pos)			{	opSp=pos+opStackBase;			}
+void opStackSetSpPos(u2 pos)	{	opSp=pos+opStackBase;		}
 
-void methodStackInit(u2** m)				{
+void methodStackInit(u2** m)	{
 #if (LINUX||AVR8||AVR32LINUX)
-	if ((*m=(u2*)calloc((size_t)METHODSTACKSIZE,sizeof(u2)))==NULL){printf("malloc error\n");exit(-1);}
+	if ((*m=(u2*)calloc((size_t)METHODSTACKSIZE,sizeof(u2)))==NULL)
+				errorExit(-1,"method stack malloc error\n");
 #else
 *m=(u2*)((u4)(apClassFileBase+MAXBYTECODE+4*MAXHEAP+4*OPSTACKSIZE+numThreads*(4*OPSTACKSIZE+2*METHODSTACKSIZE)));
 #endif
-												}
+									}
 
-void methodStackPush(u2 val)		{	*(methodSp++)=val;					}
-u2 methodStackPop()					{	return *(--methodSp);				}
-u2 methodStackPeek()				{	return *(methodSp-1);				}
-u1 methodStackEmpty()				{	return (methodSp==methodStackBase) ? 1:0;	}
-u2 methodStackGetSpPos()			{  	return (methodSp-methodStackBase);		}
+void methodStackPush(u2 val)	{	*(methodSp++)=val;		}
+u2 methodStackPop()		{	return *(--methodSp);		}
+u2 methodStackPeek()		{	return *(methodSp-1);		}
+u1 methodStackEmpty()		{	return (methodSp==methodStackBase) ? 1:0;	}
+u2 methodStackGetSpPos()	{  	return (methodSp-methodStackBase);		}
 // relative to actual base
-void methodStackSetSpPos(u2 pos)	{	methodSp=pos+methodStackBase;			}
+void methodStackSetSpPos(u2 pos){	methodSp=pos+methodStackBase;	}
