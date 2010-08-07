a0f2b61bfa4089b69b4f2a4ecee7f8649f230676
c.hrabia <c.hrabia@c12d0c00-9e51-c958-af8d-857a4e95901e>
2010-08-07 09:54:23 +0000

    nicht mehr notwendig

--- BAJOSBOOT/sources/platform/charon/Charon.java
+++
@@ -1,8 +0,0 @@
-package platform;
-
-public class Charon	{
-public Charon()	{}
-public native char nativeCharIn();
-public native void nativeCharOut(char c);
-
-}
--- BAJOSBOOT/sources/platform/charon/PlatForm.java
+++
@@ -1,8 +0,0 @@
-package platform;
-
-public class PlatForm	{
-public PlatForm()	{}
-public native char nativeCharIn();
-public native void nativeCharOut(char c);
-
-}
